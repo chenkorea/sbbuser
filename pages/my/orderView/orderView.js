3bfa3a4b4b65042a8eb3f7e519e23d79b12e3568
chenlang <978834639@qq.com>
2017-07-22 14:23:53 +0800

    dingdan
    dinged

--- pages/my/orderView/orderView.js
+++ pages/my/orderView/orderView.js
@@ -58,7 +58,7 @@ Page({
       url: '../mine/updatepwd/updatepwd',
     })
   },
-  onLoad: function () {
+  onLoad: function (options) {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
