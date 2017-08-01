f81e8fc579ef6768b80c34fa8ad06d546c318724
loujianyong1991 <15381147827@163.com>
2017-08-01 16:06:02 +0800

    通知时间格式化

--- pages/index/index.js
+++ pages/index/index.js
@@ -191,7 +191,7 @@ Page({
     noticeutil.getnotice(uid,function(e){
       if (e.data.code == '1') {
         for (var i = 0; i < e.data.content.length; i++) {
-          e.data.content[i].send_time = new Date(parseInt(e.data.content[i].send_time)).toLocaleString().replace(/[\u4E00-\u9FA5]/g, '');
+          e.data.content[i].send_time = new Date(parseInt(e.data.content[i].send_time)).toLocaleString('chinese', { hour12: false });
         }
         that.setData({
           noticecontent: e.data.content
