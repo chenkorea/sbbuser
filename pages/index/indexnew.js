177e7ff945987780f2afda27bf0e4fef17b7a760
loujianyong1991 <15381147827@163.com>
2017-08-22 17:16:52 +0800

    实时更新用户位置信息

--- pages/index/indexnew.js
+++ pages/index/indexnew.js
@@ -77,9 +77,7 @@ Page({
     })
     // 自动定位获取地理位置
     homeUtil.getCityName(function (locationData) {
-      console.log("----------");
-      console.log(locationData);
-      homeUtil.updateLocation(locationData.location.lng, locationData.location.lat, that.data.uid);
+      // homeUtil.updateLocation(locationData.location.lng, locationData.location.lat, that.data.uid);
       that.setData({
         longitude: locationData.location.lng,
         latitude: locationData.location.lat
