514e1454833db1f19957f1ee897006444b27f313
Loujianyong <15381147827@163.com>
2018-06-22 17:58:42 +0800

    锁帮帮

--- pages/index/search/persondetail/persondetail.js
+++ pages/index/search/persondetail/persondetail.js
@@ -7,6 +7,7 @@ var app = getApp()
 Page({
   data: {
     imgUrls: [],
+    companyUrls:[],
     imageWidth: wx.getSystemInfoSync().windowWidth,//图片宽度 
     indicatorDots: 3,
     name: '',
@@ -14,6 +15,7 @@ Page({
     grade: '',
     servicetype: [],
     hidePhone:'',
+    company:{},
     test:[
       'http://imge.kugou.com/stdmusic/20170423/20170423114015440967.jpg',
       'http://imge.kugou.com/stdmusic/20151218/20151218200608401755.jpg',
@@ -28,17 +30,20 @@ Page({
     if (tech_id) {
       // 查询
       that.gettechqual(tech_id);
+      that.getcompany(tech_id);
     } else {
       // 显示
       var e = JSON.parse(info.infor)
-      that.setData({
-        name: e.name,
-        phone: e.phone,
-        grade: e.grade,
-        servicetype: e.service_types,
-        imgUrls: e.archives_url,
-        hidePhone: e.phone.replace(/(\d{3})(\d{4})(\d{4})/, "$1****$3")
-      })
+      that.getcompany(e.id);
+      that.gettechqual(e.id);
+      // that.setData({
+      //   name: e.name,
+      //   phone: e.phone,
+      //   grade: e.grade,
+      //   servicetype: e.service_types,
+      //   imgUrls: e.archives_url,
+      //   hidePhone: e.phone.replace(/(\d{3})(\d{4})(\d{4})/, "$1****$3")
+      // })
     }
   },
   preview: function (e) {
@@ -46,6 +51,13 @@ Page({
     wx.previewImage({
       current: e.currentTarget.dataset.url,
       urls: this.data.imgUrls,
+    })
+  },
+  compreview: function (e) {
+    var str = JSON.stringify(e)
+    wx.previewImage({
+      current: e.currentTarget.dataset.url,
+      urls: this.data.companyUrls,
     })
   },
   changeIndicatorDots: function (e) {
@@ -68,6 +80,30 @@ Page({
       duration: e.detail.value
     })
   },
+  getcompany: function (tech_id) {
+    var that = this;
+    Util.getcompany(function (data) {
+      if (wx.hideLoading) {
+        wx.hideLoading();
+      }
+      var code = data.data.code;
+      if (code == "1") {
+        var companys = data.data.content;
+        var company = companys[0];
+        var cimgs = []
+        if (company.company_url != undefined) {
+          cimgs = company.company_url.split(",");
+          for (var i = 0; i < cimgs.length-1; i++) {
+            cimgs[i] = (getApp().globalData.imageServerIp + cimgs[i])
+          }
+        }
+        that.setData({
+          company: company,
+          companyUrls:cimgs
+        })
+      }
+    }, tech_id);
+  },
   gettechqual: function (tech_id) {
     var that = this;
     Util.gettechqual(function (data) {
@@ -79,9 +115,9 @@ Page({
         var users = data.data.content;
         var shifu = users[0];
         var imgs = []
-        if (shifu.archives_url != undefined){
+        if (shifu.archives_url != undefined) {
           imgs = shifu.archives_url.split(",");
-          for (var i = 0; i < imgs.length;i++){
+          for (var i = 0; i < imgs.length; i++) {
             imgs[i] = (getApp().globalData.imageServerIp + imgs[i])
           }
         }
@@ -92,7 +128,6 @@ Page({
           })
         }
 
-        console.log(imgs);
         that.setData({
           name: shifu.name,
           phone: shifu.phone,
